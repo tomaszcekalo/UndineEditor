4a53d99310551f62496cf2d727f7c627796da070
Tomasz Cekało <tomasz.cekalo@gmail.com>
2024-01-02 10:11:06 +0100

    added Avalonia project

---
+++ UndineEditor.Avalonia/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace UndineEditor.Avalonia
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
